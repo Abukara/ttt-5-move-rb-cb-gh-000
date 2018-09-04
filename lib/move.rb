ab43dd488c8a3829d63f93ba2252fad03fdefc83
Lukas F <lukas.forst48@googlemail.com>
2018-09-04 20:05:24 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -5,6 +5,10 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+def numeric?
+  Float(self) != nil secue false
+end
+
 
 def input_to_index(new_move)
   if new_move.numeric?
