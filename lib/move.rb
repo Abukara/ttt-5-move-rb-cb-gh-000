6d8c74864332d4fc9dbe702f6043165c95268e35
Lukas F <lukas.forst48@googlemail.com>
2018-09-04 19:59:35 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,12 +7,13 @@ def display_board(board)
 end
 
 def input_to_index(new_move)
-  
+
   array_ab = new_move.to_i
   if array_ab < 10 or array_ab > 1
     return -1
   else
-  array_c = array_ab-1
-  return array_c
+    array_c = array_ab-1
+    return array_c
+  end
 end
 # code your input_to_index and move method here!
